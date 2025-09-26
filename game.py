a5d8db67d9f1aa0ebd2c8d0cf8a4c42d488714bd
carlog1566 <cgprojects2508@gmail.com>
2025-09-25 23:46:42 -0700

    [SA-3] Create Grid Background

--- game.py
+++ game.py
@@ -14,6 +14,13 @@ pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
 game_active = True
 
+# GRID FOR AESTHETIC PURPOSES
+def drawGrid():
+    for x in range(0, WINDOW_WIDTH, BLOCK_SIZE):
+        for y in range(0, WINDOW_HEIGHT, BLOCK_SIZE):
+            grid_rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, 'gray9', grid_rect, 1)
+
 # GAMEPLAY LOOP
 while True:
     for event in pygame.event.get():
@@ -22,5 +29,8 @@ while True:
             pygame.quit()
             exit()
 
+    screen.fill('Black')
+    drawGrid()
+    
     pygame.display.update()
     clock.tick(10)
